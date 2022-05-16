4960ec888d8d84a53f3cc789070cef817aed9c7a
Alexandre Eugene <you@example.com>
2022-05-16 09:45:07 +0200

    cors fix

--- server.js
+++ server.js
@@ -12,15 +12,7 @@ const PORT = process.env.PORT || 3000
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
-app.get('/',(req,res)=>{
-    res.json('hello, heroku')
-})
+app.use(cors())
 
 app.use(router)
 app.listen(PORT, () => {
