3576642030e57347c427d5db6438f2b37987e7bc
Alexandre Eugene <you@example.com>
2022-05-16 09:49:14 +0200

    cors fix3

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ const PORT = process.env.PORT || 3000
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-app.use(cors({ origin: "https://my-imcs.netlify.app", credentials: true }))
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 
 app.use(router)
 app.listen(PORT, () => {
