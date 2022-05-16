dfb1227884749f37463474b51b840de3d0cb3288
Alexandre Eugene <you@example.com>
2022-05-16 09:47:31 +0200

    cors fix2

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-app.use(cors())
+app.use(cors({ origin: "https://my-imcs.netlify.app", credentials: true }))
 
 app.use(router)
 app.listen(PORT, () => {
