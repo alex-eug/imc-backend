5e19fd9ecb362e10807f394767233beb182ff132
Alexandre Eugene <you@example.com>
2022-05-16 10:16:52 +0200

    cors fix7

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin',"https://my-imcs.netlify.app/showImc", '*');
+    res.setHeader('Access-Control-Allow-Origin',"https://my-imcs.netlify.app");
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
