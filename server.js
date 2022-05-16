1e2a8a524d60dfc2959ff2e3a5f76f50ad97b934
Alexandre Eugene <you@example.com>
2022-05-16 10:22:57 +0200

    cors fix8

--- server.js
+++ server.js
@@ -13,12 +13,14 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin',"https://my-imcs.netlify.app");
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
-
+app.get('/',(req,res)=>{
+    res.json('hello, heroku')
+})
 
 app.use(router)
 app.listen(PORT, () => {
